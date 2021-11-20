c9d9920078e461e798bc46c34a08c632e7ec8f76
Jonny Saykosy <josayko@pm.me>
2021-11-20 17:09:16 +0100

    Add working carousel

--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
-const axios = require('axios');
+const axios = require('axios').default;
 
-const baseUrl = 'http://localhost:8000/api/v1/titles/';
+const baseUrl = 'http://localhost:8000/api/v1/titles?page=1&year=2004';
+const baseUrl2 = 'http://localhost:8000/api/v1/titles?page=2&year=2004';
 
-const add_movie = (data) => {
-  const newDiv = document.createElement('div');
-  let el = document.createElement('h2');
+const sliders = document.querySelector('.carousel-box');
+const switchLeft = document.querySelector('.switchLeft');
+const switchRight = document.querySelector('.switchRight');
 
-  el.textContent = data[0].title;
-  newDiv.appendChild(el);
+var scrollPerClick;
+var scrollAmount = 0;
 
-  el = document.createElement('img');
-  el.setAttribute('src', data[0].image_url);
-  newDiv.appendChild(el);
+showMovieData();
 
-  document.body.appendChild(newDiv);
-};
-
-axios
-  .get(baseUrl)
-  .then((res) => {
-    console.log(res);
-    add_movie(res.data.results);
-  })
-  .then((err) => {
-    console.log(err);
+switchLeft.addEventListener('click', () => {
+  sliders.scrollTo({
+    top: 0,
+    left: (scrollAmount -= scrollPerClick),
+    behavior: 'smooth'
   });
+  if (scrollAmount < 0) {
+    scrollAmount = 0;
+  }
+});
+
+switchRight.addEventListener('click', () => {
+  if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth) {
+    sliders.scrollTo({
+      top: 0,
+      left: (scrollAmount += scrollPerClick),
+      behavior: 'smooth'
+    });
+  }
+});
+
+async function showMovieData() {
+  let total = [];
+  let result = await axios.get(baseUrl);
+  let result2 = await axios.get(baseUrl2);
+  result = result.data.results;
+  result2 = result2.data.results;
+  total.push(...result);
+  total.push(...result2);
+  console.log(total);
+
+  total.map((cur, index) => {
+    sliders.insertAdjacentHTML(
+      'beforeend',
+      `<img class="img-${index} slider-img" src="${cur.image_url}" />`
+    );
+  });
+
+  scrollPerClick = 500;
+}
