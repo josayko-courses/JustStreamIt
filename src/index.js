af3014af7d5d385f75da0ca66a4faa9386727125
Jonny Saykosy <josayko@pm.me>
2021-11-27 12:46:10 +0100

    Add modal header + footer

--- src/index.js
+++ src/index.js
@@ -59,8 +59,20 @@ const carouselBox = document.querySelector('.carousel-box');
 const closeBtn = document.querySelector('.closeBtn');
 
 carouselBox.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
 
 function openModal(e) {
   modal.style.display = 'block';
   console.log(e);
+}
+
+function closeModal(e) {
+  modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
 }
